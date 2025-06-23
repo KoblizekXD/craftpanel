a766b85dd61cf49a9cca8d8a54d393e0ee1eb687
KoblizekXD <janprokupek@centrum.cz>
2025-06-23 11:14:23 +0200

    feat: add sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,9 +1,26 @@
 import ServerContextProvider from "@/components/server-context-provider";
+import DashboardSidebar from "@/components/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ServerContextProvider>{children}</ServerContextProvider>;
+  return (
+    <ServerContextProvider>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }>
+        <DashboardSidebar />
+        <main className="flex w-full min-h-screen flex-col items-center justify-between p-8 md:p-24">
+          {children}
+        </main>
+      </SidebarProvider>
+    </ServerContextProvider>
+  );
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { Info, Server, Settings, Terminal } from "lucide-react";
+import Link from "next/link";
+import { useServerContext } from "@/lib/server-context";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "./ui/sidebar";
+
+const dockerOptionsFields = Object.entries({
+  socketPath: "Socket Path",
+  host: "Host",
+  port: "Port",
+  username: "Username",
+  headers: "Headers",
+  ca: "Certificate Authority (CA)",
+  cert: "Client Certificate",
+  key: "Private Key",
+  protocol: "Protocol (http | https | ssh)",
+  timeout: "Timeout (ms)",
+  version: "API Version",
+  sshAuthAgent: "SSH Auth Agent",
+  sshOptions: "SSH Options (ConnectConfig)",
+});
+
+export function DashboardSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  const serverContext = useServerContext();
+
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg">
+              <HoverCard>
+                <HoverCardTrigger asChild>
+                  <Link href="/" className="flex items-center gap-2">
+                    <Server />
+                    <div className="flex flex-col">
+                      <span className="font-semibold">Craftpanel</span>
+                      <span className="text-xs text-muted-foreground">
+                        (Hover for status)
+                      </span>
+                    </div>
+                  </Link>
+                </HoverCardTrigger>
+                <HoverCardContent className="text-sm w-fit">
+                  <p className="font-semibold">Connection details:</p>
+                  {Object.entries(serverContext?.connectionDetails || {}).map(
+                    (k, v) => (
+                      <span key={k[0]}>
+                        {dockerOptionsFields[v][1]}: {k[1]}
+                      </span>
+                    ),
+                  )}
+                </HoverCardContent>
+              </HoverCard>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href={"/overview"} className="font-medium">
+                  <Info />
+                  Overview
+                </Link>
+              </SidebarMenuButton>
+              <SidebarMenuSub>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href={"/analytics"}>Analytics</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+              </SidebarMenuSub>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href={"/servers"} className="font-medium">
+                  <Terminal />
+                  Servers
+                </Link>
+              </SidebarMenuButton>
+              <SidebarMenuSub></SidebarMenuSub>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href={"/settings"} className="font-medium">
+                  <Settings />
+                  Settings
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
+
+export default DashboardSidebar;
