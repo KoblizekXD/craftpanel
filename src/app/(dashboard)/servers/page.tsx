f09d91a74db686ec09ee46236269973d08858437
KoblizekXD <janprokupek@centrum.cz>
2025-06-26 18:44:15 +0200

    feat: fix alignment on loading bar

--- src/app/(dashboard)/servers/page.tsx
+++ src/app/(dashboard)/servers/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { LoaderCircle, Plus } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import StatusBadge from "@/components/status-badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -16,9 +16,10 @@ import { useServerContext } from "@/lib/server-context";
 
 export default function ServersPage() {
   const servers = useServerContext();
+  const router = useRouter();
 
   return (
-    <div className="flex flex-col gap-y-7">
+    <div className="flex flex-col gap-y-7 h-full">
       <h1 className="text-2xl font-bold">Servers</h1>
       <div className="flex items-center">
         <Button className="flex items-center gap-x-1">
@@ -27,7 +28,7 @@ export default function ServersPage() {
         </Button>
       </div>
       {servers?.loading ? (
-        <div className="flex items-center gap-x-2 justify-center">
+        <div className="flex items-center gap-x-2 justify-center h-full">
           <p className="text-sm text-muted-foreground">
             Loading servers, please wait...
           </p>
@@ -44,15 +45,12 @@ export default function ServersPage() {
           </TableHeader>
           <TableBody>
             {servers?.servers.map((server) => (
-              <TableRow key={server.id}>
+              <TableRow
+                className="cursor-pointer"
+                onClick={() => router.push(`/servers/${server.id}`)}
+                key={server.id}>
                 <TableCell>{server.id.substring(0, 12)}</TableCell>
-                <TableCell>
-                  <Link
-                    href={`/servers/${server.id}`}
-                    className="hover:underline">
-                    {server.name}
-                  </Link>
-                </TableCell>
+                <TableCell>{server.name}</TableCell>
                 <TableCell className="text-right">
                   <StatusBadge status={server.status} />
                 </TableCell>
